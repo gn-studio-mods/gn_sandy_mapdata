8cc71c7ee75b7b388f40ad0207434da71109614c
G&N's Studio <111233935+gn-studio-mods@users.noreply.github.com>
2023-07-24 16:50:27 +0200

    Push 2
    GN_Sheriff_Dept_Only + GN_Sandy_Motel_Only + GN_Clinic-Center_Only

---
+++ GN_Sheriff_Dept_Only/cfx_gn_sandy_mapdata/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version "cerulean"
+games { 'gta5' }
+
+author 'G&N_s Studio'
+description 'Sandy Shore Mapdata - Sheriff Only'
+version '1.0.1'
+
+dependencies {
+    '/server:4960',
+    '/gameBuild:2189',
+}
+
+this_is_a_map 'yes'
+
+escrow_ignore {
+    'stream/ybn/*.ybn',
+    'stream/ydr/*.ydr',
+    'stream/ymap/*.ymap',
+    'stream/ymap/*.ydd'
+}
